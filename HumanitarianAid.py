00ea545cb21a12aebcb40aa060a1a6fd65625011
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-05-15 16:37:53 +0300

    add def_get_number_of_trucks

--- HumanitarianAid.py
+++ HumanitarianAid.py
@@ -22,8 +22,20 @@
 MAXIMUM_NUMBER_OF_CONTAINERS_IN_A_TRUCK = 10
 MAXIMUM_BOXES_IN_THE_CONTAINER = 25
 
-try:
-    boxes = int(input('введите кол-во ящиков'))
-    print(boxes)
-except ValueError:
-    print('введите целое число')
+
+def get_number_of_trucks(amount):
+    number_of_container = round(amount/MAXIMUM_BOXES_IN_THE_CONTAINER, 0)
+    number_of_truck = number_of_container/MAXIMUM_NUMBER_OF_CONTAINERS_IN_A_TRUCK
+
+    print('кол-во контейнеров', number_of_container)
+    print('кол-во грузовиков', number_of_truck)
+
+
+while True:
+    try:
+        boxes = int(input('введите кол-во ящиков'))
+        print(boxes)
+    except ValueError:
+        print('введите целое число')
+    get_number_of_trucks(boxes)
+
