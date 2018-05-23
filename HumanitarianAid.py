f714063bdc1549e6d7ddf734378ffc45394b83be
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-05-23 20:10:39 +0300

    add def out_boxes, def out_trucks

--- HumanitarianAid.py
+++ HumanitarianAid.py
@@ -23,10 +23,23 @@ MAXIMUM_NUMBER_OF_CONTAINERS_IN_A_TRUCK = 10
 MAXIMUM_BOXES_IN_THE_CONTAINER = 25
 
 
+def out_boxes(containers):
+    for container in range(1, containers + 1):
+        print('контейнер номер {}'.format(container))
+        if container == containers + 1:
+            pass
+
+def out_trucks(trucks, boxes):
+    for truck in range(1, trucks + 1):
+        print('грузовик номер {}'.format(truck))
+        out_boxes(boxes)
+
+
 def get_number_of_trucks(amount):
     number_of_container = round(amount/MAXIMUM_BOXES_IN_THE_CONTAINER, 0)
     number_of_truck = number_of_container/MAXIMUM_NUMBER_OF_CONTAINERS_IN_A_TRUCK
 
+    out_trucks(int(number_of_truck), int(number_of_container))
     print('кол-во контейнеров', number_of_container)
     print('кол-во грузовиков', number_of_truck)
 
