463220cc8e3f2d5af63806c025533ca8034f930b
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-05-23 20:55:01 +0300

    del def out_boxes, def out_trucks add out_all

--- HumanitarianAid.py
+++ HumanitarianAid.py
@@ -23,23 +23,23 @@ MAXIMUM_NUMBER_OF_CONTAINERS_IN_A_TRUCK = 10
 MAXIMUM_BOXES_IN_THE_CONTAINER = 25
 
 
-def out_boxes(containers):
-    for container in range(1, containers + 1):
-        print('контейнер номер {}'.format(container))
-        if container == containers + 1:
-            pass
-
-def out_trucks(trucks, boxes):
+def out_all(trucks, conteiners, all_boxes):
+    amount = 0
     for truck in range(1, trucks + 1):
         print('грузовик номер {}'.format(truck))
-        out_boxes(boxes)
+        for container in range(1, conteiners + 1):
+            print('контейнер номер {}'.format(container))
+            for number in range(MAXIMUM_BOXES_IN_THE_CONTAINER):
+                amount += 1
+                if amount <= all_boxes:
+                    print('ящик номер ', amount)
 
 
 def get_number_of_trucks(amount):
     number_of_container = round(amount/MAXIMUM_BOXES_IN_THE_CONTAINER, 0)
     number_of_truck = number_of_container/MAXIMUM_NUMBER_OF_CONTAINERS_IN_A_TRUCK
 
-    out_trucks(int(number_of_truck), int(number_of_container))
+    out_all(int(number_of_truck), MAXIMUM_NUMBER_OF_CONTAINERS_IN_A_TRUCK, int(amount))
     print('кол-во контейнеров', number_of_container)
     print('кол-во грузовиков', number_of_truck)
 
