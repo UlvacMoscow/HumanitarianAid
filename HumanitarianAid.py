a1b60c1a50d32c44a5fff44444310913b8b20c06
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2018-05-23 21:02:15 +0300

    update out_all, get_number_of_trucks

--- HumanitarianAid.py
+++ HumanitarianAid.py
@@ -1,33 +1,16 @@
-# - Вы доставляете гуманитарную помощь в виде ящиков одинакового размера. У вас есть грузовики и контейнеры.
-# В каждый грузовик влезает максимум 10 контейнеров. В каждый контейнер ­ не более 25 ящиков.
-# Ящики, контейнеры и грузовики пронумерованы.
-# Напишите программу, которая будет распределять ящики по контейнерам и грузовикам в зависимости от их количества,
-# а также напечатает, сколько всего нужно грузовиков и контейнеров.
-# На вход программы через консоль должно подаваться число ящиков.
-# На выходе результат должен выглядеть следующим образом:
-#
-# Грузовик 1:
-#
-# Контейнер 1:
-#
-# Контейнер 2
-#
-# Ящик 1
-#
-# Ящик 2
-#
-# Ящик 3
-
+import math
 
 MAXIMUM_NUMBER_OF_CONTAINERS_IN_A_TRUCK = 10
 MAXIMUM_BOXES_IN_THE_CONTAINER = 25
 
 
-def out_all(trucks, conteiners, all_boxes):
+def out_all(trucks, containers, all_boxes):
     amount = 0
     for truck in range(1, trucks + 1):
         print('грузовик номер {}'.format(truck))
-        for container in range(1, conteiners + 1):
+        for container in range(1, containers + 1):
+            if amount > all_boxes:
+                break
             print('контейнер номер {}'.format(container))
             for number in range(MAXIMUM_BOXES_IN_THE_CONTAINER):
                 amount += 1
@@ -36,9 +19,8 @@ def out_all(trucks, conteiners, all_boxes):
 
 
 def get_number_of_trucks(amount):
-    number_of_container = round(amount/MAXIMUM_BOXES_IN_THE_CONTAINER, 0)
-    number_of_truck = number_of_container/MAXIMUM_NUMBER_OF_CONTAINERS_IN_A_TRUCK
-
+    number_of_container = math.ceil(amount/MAXIMUM_BOXES_IN_THE_CONTAINER)
+    number_of_truck = math.ceil(number_of_container/MAXIMUM_NUMBER_OF_CONTAINERS_IN_A_TRUCK)
     out_all(int(number_of_truck), MAXIMUM_NUMBER_OF_CONTAINERS_IN_A_TRUCK, int(amount))
     print('кол-во контейнеров', number_of_container)
     print('кол-во грузовиков', number_of_truck)
